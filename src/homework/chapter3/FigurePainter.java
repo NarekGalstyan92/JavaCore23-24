20b987136bb41fc7cb67a987d20334aee3360138
narekgalstyan <narekgalstyan92@gmail.com>
2023-08-27 22:28:31 -0700

    Painting figures with stars

---
+++ src/homework/chapter3/FigurePainter.java
@@ -0,0 +1,86 @@
+package homework.chapter3;
+
+class FigurePainter {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        /* Output:
+
+         *
+         * *
+         * * *
+         * * * *
+         * * * * *
+
+         */
+
+        System.out.println("\n");
+
+        for (int i = 5; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        /* Output:
+
+         * * * * *
+         * * * *
+         * * *
+         * *
+         *
+
+         */
+
+        System.out.println("\n");
+
+        int line = 5;
+        for (int i = 0; i < line; i++) {
+            for (int j = i + 1; j < line; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        /* Output:
+
+                 *
+               * *
+             * * *
+           * * * *
+         * * * * *
+
+         */
+
+        System.out.println("\n");
+
+        for (int i = line; i > 0; i--) {
+            for (int j = i; j < line; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 0; k < i; k++) {
+                System.out.print("* ");
+            }
+
+
+            System.out.println();
+        }
+        /* Output:
+
+         * * * * *
+           * * * *
+             * * *
+               * *
+                 *
+
+         */
+    }
+
+}
+
