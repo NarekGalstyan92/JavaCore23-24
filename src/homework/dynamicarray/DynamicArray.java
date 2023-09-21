bc0a252eb134f2028bb91b4fdce3ebe663305641
narekgalstyan <narekgalstyan92@gmail.com>
2023-09-21 16:28:43 -0700

    DynamicArray upgrade. Added deleteByIndex(), set(), add(index, value), exists() and getIndexByValue() methods

--- src/homework/dynamicarray/DynamicArray.java
+++ src/homework/dynamicarray/DynamicArray.java
@@ -11,6 +11,7 @@ public class DynamicArray {
         }
         array[size++] = value;
         System.out.println("Value " + value + " added to array");
+        System.out.println();
     }
 
     // adding small integers into array using iteration.
@@ -31,7 +32,7 @@ public class DynamicArray {
                 extend();
             }
             array[size++] = i;
-            System.out.println("Value " + i + " added to array");
+            System.out.println("Value " + i + " added to array.");
         }
     }
 
@@ -45,18 +46,21 @@ public class DynamicArray {
             temp[i] = array[i];
         }
         array = temp;
-        System.out.println("\nThe array was extended\n");
+        System.out.println("\nThe array was extended.\n");
+        System.out.println();
         temp = null; // losing the reference of temp, so GC can sure clean the heap
     }
 
-    // if the given index is within the index of the array we have,
+    // if the given index is within the range of the array,
     // return the element under that index. Otherwise, return -1.
     public int getByIndex(int index) {
-        if (index >= 0 && index < size) {
-            System.out.println("Value under index " + index + " is returned, and it was " + array[index] + "\n");
+        if (checkIndexExisting(index)) {
+            System.out.println("Value under index " + index + " is returned, and it was " + array[index] + ".\n");
             return array[index];
         }
-        System.out.println("Index is out of bounds for array length");
+
+        System.out.println("Couldn't get value with given index! There is no such element under index " + index + ".");
+        System.out.println();
         return -1;
     }
 
@@ -67,6 +71,88 @@ public class DynamicArray {
             System.out.print(array[i] + " ");
         }
         System.out.println();
+    }
+
+    //delete the element of the array under given index
+    //if there is no element with such index, print that there is no such element
+    public void deleteByIndex(int index) {
+        if (!checkIndexExisting(index)) {
+            System.out.println("Value deleting failed! There is no such element under index " + index + ".");
+        } else {
+            for (int i = index; i < size - 1; i++) {
+                array[i] = array[i + 1];
+            }
+            array[size - 1] = 0; // assigning last element of the array 0 to clear the given index from data
+            size--; // changing size length to lose last element
+            System.out.println("Value under index " + index + " was deleted successfully!");
+            System.out.println("Array was updated!");
+            print();
+            System.out.println();
+        }
+    }
+
+    // Set the value under given index.
+    // If there is no element with such index, print that there is no such element
+    public void set(int index, int value) {
+        if (!checkIndexExisting(index)) {
+            System.out.println("Setting value \"" + value + "\" with given index failed! There is no such element under index " + index + ".");
+        } else {
+            array[index] = value;
+            System.out.println("Value ander index " + index + " was changed successfully!");
+            System.out.println("Array was updated.");
+            print();
+            System.out.println();
+        }
+    }
+
+    //Add a value into array with given index, move other elements to the right
+    // If there is no element with such index, print that there is no such element
+    public void add(int index, int value) {
+        if (!checkIndexExisting(index)) {
+            System.out.println("Adding value \"" + value + "\" with given index failed! There is no such element under index " + index + ".");
+            System.out.println();
+        } else {
+            size++; //enlarging the size so elements in array can be moved to the right
+            if (size >= array.length) {
+                extend();
+            }
+            for (int i = size; i > index; i--) { // moving elements to the right
+                array[i] = array[i - 1];
+            }
+            array[index] = value; // setting value into given index
+            System.out.println("Array was updated!");
+            print();
+            System.out.println();
+        }
+    }
+
+    //Return true if the value exist in array and false if it doesn't
+    public boolean exists(int value) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value) {
+                System.out.println("Value \"" + value + "\" exist!\n");
+                return true;
+            }
+        }
+        System.out.println("Value \"" + value + "\" doesn't exist!\n");
+        return false;
+    }
+
+    //Return index of given value. If there are many - return first index. If there isn't any matching - return -1.
+    public int getIndexByValue(int value) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value) {
+                System.out.println("Index returned (" + i + ")\n");
+                return i;
+            }
+        }
+        System.out.println("Couldn't get any matching with given value \"" + value + "\".");
+        return -1;
+    }
+
+    // checking if the given index is in the range from 0 to size.
+    private boolean checkIndexExisting(int index) {
+        return index >= 0 && index < size;
     }
 }
 
--- src/homework/dynamicarray/DynamicArrayDemo.java
+++ src/homework/dynamicarray/DynamicArrayDemo.java
@@ -5,10 +5,15 @@ public class DynamicArrayDemo {
 
         DynamicArray dy = new DynamicArray();
         dy.addSmallNumbers(10); // using iteration adding numbers into array, value is the length
-        dy.addLargeNumbers(500); // using iteration adding three-digit number, start point is 100. Increments by 100
+        dy.addLargeNumbers(501); // using iteration adding three-digit number, start point is 100. Increments by 100, value is the length
         dy.add(-74); // simply adding a number into array
         dy.print(); // printing all values of the array
-        dy.getByIndex(15);
+        dy.getByIndex(0); //getting the value under given index
+        dy.deleteByIndex(10); // deleting value with given index, moving to the left all other values, shortening size
+        dy.set(2, 700); // Setting the value with given index in the array
+        dy.add(0, 22); // adding value with given index, moving to the right all other values, enlarging size
+        dy.exists(3); // checking if the value exist in array
+        dy.getIndexByValue(500); // Return index of given value. If there are many - return first index
 
     }
 }
