2434263cf29d92d11891613ab13cc41209a46dc1
narekgalstyan <narekgalstyan92@gmail.com>
2023-11-25 22:23:05 -0800

    chapter14 compareable and comparator

---
+++ src/classwork/chapter14/compare/DateComparator.java
@@ -0,0 +1,12 @@
+package classwork.chapter14.compare;
+
+
+import java.util.Comparator;
+
+public class DateComparator implements Comparator<Student> {
+
+    @Override
+    public int compare(Student o1, Student o2) {
+        return o1.getRegisterDate().compareTo(o2.getRegisterDate());
+    }
+}
